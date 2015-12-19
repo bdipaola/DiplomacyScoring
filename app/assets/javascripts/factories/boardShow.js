4ecc6280adf759c4a60508878f02ab9f25a8abc5
[01;31m[K"name"[m[K: "Ben DiPaola <[01;31m[K"email"[m[K>
2015-12-18 19:57:14 -0600

    Add function updates to edit game board

--- app/assets/javascripts/factories/boardShow.js
+++ app/assets/javascripts/factories/boardShow.js
@@ -1,18 +1,35 @@
 angular.module("DiplomacyApp")
 .factory('boardShow', ['$http', '$location', function($http, $location){
   
-  var boardData = { data: { board_number: 'Loading', board_location: 'Loading', players: [] }, isLoaded: false };
+  var boardData = { 
+    data: { 
+      board_number: 'Loading', board_location: 'Loading', players: [], 
+      isLoaded: false 
+    }
+  };
 
   boardData.loadBoard = function(boardId){
     $http.get('/boards/' + boardId + '.json')
     .success(function(data){
     	boardData.data = data;
-    	boardData.isLoaded = true;
+    	boardData.data.isLoaded = true;
       console.log('Successfully loaded board and player data.');
     })
     .error(function(){
       console.error('Failed to load board and player data.');
     });
 	}
+
+  boardData.updateBoard = function(boardData) {
+    $http.put('/boards/' + boardData.board_number + '.json', boardData)
+    .then(function(data){
+      console.log("Successfully updated board.")
+      $location.url('/boards/' + boardData.board_number);
+    },
+    function(){
+      console.error('Failed to update board.');
+    });
+  }
+
   return boardData;
 }]);
